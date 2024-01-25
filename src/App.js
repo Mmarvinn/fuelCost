4b166dc69774e4655be2ad28eccc7feb1612aaac
Anatolii Tserkunyk <cerkunik@gmail.com>
2024-01-25 16:38:26 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { RootLayout } from './components/RootLayout';
+
+function App() {
+  return (
+    <main className='App'>
+      <RootLayout></RootLayout>
+    </main>
+  );
+}
+
+export default App;
